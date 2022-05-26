2c717bbb44f99e86939575b18aa7b0d7c275bd27
KIRANA\kirana <kirana0701@gmail.com>
2022-05-26 21:47:44 +0700

    chart & static input

---
+++ components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './footer.module.css'
+
+export default function Footer() {
+  return (
+    <div className={styles.footer}>
+        FEJS 2 | Chapter 7
+    </div>
+  )
+}
